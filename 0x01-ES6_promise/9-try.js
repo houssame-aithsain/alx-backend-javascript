6e5d87b3f30b1b1fb21620b5b55d162fbc046dfb
Houssame <hossame@inbox.ru>
2024-10-02 21:17:54 +0100

    9

--- 0x01-ES6_promise/9-try.js
+++ 0x01-ES6_promise/9-try.js
@@ -1,16 +1,16 @@
 export default function guardrail(mathFunction) {
-  const queue = []; // Initialize an empty array to store results
+  const container = []; // Initialize an empty array to store results
 
   try {
-    // Attempt to execute the mathFunction and push the result to the queue
-    queue.push(mathFunction());
+    // Attempt to execute the mathFunction and push the result to the container
+    container.push(mathFunction());
   } catch (error) {
-    // If an error occurs, push the error message to the queue
-    queue.push(String(error));
+    // If an error occurs, push the error message to the container
+    container.push(String(error));
   } finally {
     // In all cases, indicate that the guardrail has been processed
-    queue.push('Guardrail was processed');
+    container.push('Guardrail was processed');
   }
 
-  return queue; // Return the resulting array
+  return container; // Return the resulting array
 }
