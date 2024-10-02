03b93eddf447af1b8aab31fac88064c37f87b1ed
Houssame <hossame@inbox.ru>
2024-10-02 21:01:42 +0100

    2

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,9 +1,14 @@
 export default function handleResponseFromAPI(promise) {
   return promise
-    .then(() => ({
-      status: 200,
-      body: 'Success',
-    }))
-    .catch(() => Error())
-    .finally(() => console.log('Got a response from the API'));
+    .then(() => {
+      console.log('Got a response from the API'); // Log the message when resolved
+      return {
+        status: 200,
+        body: 'success',
+      };
+    })
+    .catch(() => {
+      console.log('Got a response from the API'); // Log the message when rejected
+      return new Error(); // Return an empty Error object on rejection
+    });
 }
