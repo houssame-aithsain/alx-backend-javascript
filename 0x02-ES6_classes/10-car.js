c328f177ee5105bbe9c8549bba118eabdd02e7fe
Houssame <hossame@inbox.ru>
2024-10-03 21:21:27 +0100

    10

---
+++ 0x02-ES6_classes/10-car.js
@@ -0,0 +1,19 @@
+const cloneSymbol = Symbol('cloneCar');
+
+class Car {
+  constructor(brand, motor, color) {
+    this._brand = brand;
+    this._motor = motor;
+    this._color = color;
+  }
+
+  [cloneSymbol]() {
+    return new this.constructor();
+  }
+
+  cloneCar() {
+    return this[cloneSymbol]();
+  }
+}
+
+export default Car;
