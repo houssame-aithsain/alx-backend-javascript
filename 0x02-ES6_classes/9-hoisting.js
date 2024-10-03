124628dbe123b3c0f7868029beabf018e51845cd
Houssame <hossame@inbox.ru>
2024-10-03 21:25:07 +0100

    9

--- 0x02-ES6_classes/9-hoisting.js
+++ 0x02-ES6_classes/9-hoisting.js
@@ -1,3 +1,4 @@
+// Define the HolbertonClass with year and location attributes
 export class HolbertonClass {
   constructor(year, location) {
     this._year = year;
@@ -13,11 +14,16 @@ export class HolbertonClass {
   }
 }
 
+// Create instances of HolbertonClass for different years
+const class2019 = new HolbertonClass(2019, 'San Francisco');
+const class2020 = new HolbertonClass(2020, 'San Francisco');
+
+// Define the StudentHolberton class
 export class StudentHolberton {
   constructor(firstName, lastName, holbertonClass) {
     this._firstName = firstName;
     this._lastName = lastName;
-    this._holbertonClass = holbertonClass;
+    this._holbertonClass = holbertonClass; // Assign the holbertonClass parameter
   }
 
   get fullName() {
@@ -25,7 +31,7 @@ export class StudentHolberton {
   }
 
   get holbertonClass() {
-    return this._holbertonClass;
+    return this._holbertonClass; // Return the stored holbertonClass
   }
 
   get fullStudentDescription() {
@@ -33,13 +39,12 @@ export class StudentHolberton {
   }
 }
 
-const class2019 = new HolbertonClass(2019, 'San Francisco');
-const class2020 = new HolbertonClass(2020, 'San Francisco');
-
+// Create instances of StudentHolberton
 const student1 = new StudentHolberton('Guillaume', 'Salva', class2020);
 const student2 = new StudentHolberton('John', 'Doe', class2020);
 const student3 = new StudentHolberton('Albert', 'Clinton', class2019);
 const student4 = new StudentHolberton('Donald', 'Bush', class2019);
 const student5 = new StudentHolberton('Jason', 'Sandler', class2019);
 
+// Export the list of students
 export const listOfStudents = [student1, student2, student3, student4, student5];
