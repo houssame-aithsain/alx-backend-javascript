696fac26c34d2bb769d89dd22c10e530eb2f4407
Elpira <elpira@elpira.168.1.36>
2024-11-26 20:10:41 +0100

    1

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,14 +1,10 @@
-// 1-stdin.js
-
 process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
 process.stdin.on('readable', () => {
   const chunk = process.stdin.read();
 
   if (chunk) {
-    // Remove any extra newlines or spaces from the input
-    const name = chunk.trim();
-    process.stdout.write(`Your name is: ${name}`);
+    process.stdout.write(`Your name is: ${chunk}`);
   }
 });
 
