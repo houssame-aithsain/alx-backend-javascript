054efd04d342e6a0f70a53ebcdbce63d69feaf72
Elpira <elpira@elpira.168.1.36>
2024-11-28 23:21:09 +0100

    8

--- 0x06-unittests_in_js/8-api/api.js
+++ 0x06-unittests_in_js/8-api/api.js
@@ -1,14 +1,13 @@
+// 8-api/api.js
 const express = require('express');
-
 const app = express();
-const PORT = 7865;
 
-app.get('/', (_, res) => {
-  res.send('Welcome to the payment system');
+// Route for GET /
+app.get('/', (req, res) => {
+  res.status(200).send('Welcome to the payment system');
 });
 
-app.listen(PORT, () => {
-  console.log(`API available on localhost port ${PORT}`);
+// Start the server on port 7865
+app.listen(7865, () => {
+  console.log('API available on localhost port 7865');
 });
-
-module.exports = app;
