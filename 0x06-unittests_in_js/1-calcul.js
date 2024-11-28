53d8d0850e3e262dc9484361b039c505407362f9
Elpira <elpira@elpira.168.1.36>
2024-11-28 22:39:13 +0100

    1

--- 0x06-unittests_in_js/1-calcul.js
+++ 0x06-unittests_in_js/1-calcul.js
@@ -1,20 +1,14 @@
-function calculateNumber(type, a, b) {
-    const roundedA = Math.round(a);
-    const roundedB = Math.round(b);
-  
+const calculateNumber = (type, a, b) => {
     if (type === 'SUM') {
-      return roundedA + roundedB;
-    } else if (type === 'SUBTRACT') {
-      return roundedA - roundedB;
-    } else if (type === 'DIVIDE') {
-      if (roundedB === 0) {
-        return 'Error';
-      }
-      return roundedA / roundedB;
-    } else {
-      throw new Error('Invalid type');
+      return Math.round(a) + Math.round(b);
     }
-  }
+    if (type === 'SUBTRACT') {
+      return Math.round(a) - Math.round(b);
+    }
+    if (type === 'DIVIDE') {
+      return Math.round(b) === 0 ? 'Error' : Math.round(a) / Math.round(b);
+    }
+    return 0;
+};
   
-  module.exports = calculateNumber;
-  
+module.exports = calculateNumber;
