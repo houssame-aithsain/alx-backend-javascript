c3a68e10ddec1678f1192d43c1635cff6a7e0fb9
Elpira <elpira@elpira.168.1.36>
2024-11-28 22:30:48 +0100

    0

--- 0x06-unittests_in_js/0-calcul.js
+++ 0x06-unittests_in_js/0-calcul.js
@@ -1,8 +1,3 @@
-// 0-calcul.js
-function calculateNumber(a, b) {
-    return Math.round(a) + Math.round(b);
-}
-  
-/* eslint-disable no-undef */
+const calculateNumber = (a, b) => Math.round(a) + Math.round(b);
+
 module.exports = calculateNumber;
-/* eslint-enable no-undef */
