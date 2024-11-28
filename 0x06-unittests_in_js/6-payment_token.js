a9534af95fe12c29940ccfa6be06c7008c232434
Elpira <elpira@elpira.168.1.36>
2024-11-28 23:09:00 +0100

    6

--- 0x06-unittests_in_js/6-payment_token.js
+++ 0x06-unittests_in_js/6-payment_token.js
@@ -1,7 +1,12 @@
-const getPaymentTokenFromAPI = (success) => new Promise((resolve, _reject) => {
-    if (success) {
-      resolve({data: 'Successful response from the API'});
-    }
-  });
-  
-module.exports = getPaymentTokenFromAPI; // Export the function for testing
+// 6-payment_token.js
+function getPaymentTokenFromAPI(success) {
+    return new Promise((resolve, reject) => {
+      if (success) {
+        resolve({ data: 'Successful response from the API' });
+      } else {
+        // Do nothing if success is false
+      }
+    });
+}
+
+module.exports = getPaymentTokenFromAPI;
--- 0x06-unittests_in_js/6-payment_token.test.js
+++ 0x06-unittests_in_js/6-payment_token.test.js
@@ -1,26 +1,12 @@
-// 6-payment_token.test.js
-const chai = require('chai');
+const { expect } = require('chai');
 const getPaymentTokenFromAPI = require('./6-payment_token');
 
-const { expect } = chai;
-
 describe('getPaymentTokenFromAPI', () => {
-  it('should resolve with the correct data when success is true', (done) => {
+  it('getPaymentTokenFromAPI(success), where success == true', (done) => {
     getPaymentTokenFromAPI(true)
-      .then((response) => {
-        expect(response).to.deep.equal({ data: 'Successful response from the API' });
-        done(); // Call done() to indicate that the test is complete
-      })
-      .catch((error) => done(error)); // If an error occurs, pass it to done()
-  });
-
-  it('should do nothing when success is false', (done) => {
-    getPaymentTokenFromAPI(false)
-      .then((response) => {
-        // No response should be received
-        expect(response).to.be.undefined;
-        done(); // Call done() to indicate that the test is complete
-      })
-      .catch((error) => done(error)); // If an error occurs, pass it to done()
+      .then((res) => {
+        expect(res).to.deep.equal({data: 'Successful response from the API'});
+        done();
+      });
   });
 });
