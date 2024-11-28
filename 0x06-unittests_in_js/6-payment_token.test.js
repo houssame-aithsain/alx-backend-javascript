9b00f28e2461b34770bdf8daa0e4a34b62e4d193
Elpira <elpira@elpira.168.1.36>
2024-11-28 23:10:22 +0100

    6

--- 0x06-unittests_in_js/6-payment_token.test.js
+++ 0x06-unittests_in_js/6-payment_token.test.js
@@ -2,11 +2,8 @@ const { expect } = require('chai');
 const getPaymentTokenFromAPI = require('./6-payment_token');
 
 describe('getPaymentTokenFromAPI', () => {
-  it('getPaymentTokenFromAPI(success), where success == true', (done) => {
-    getPaymentTokenFromAPI(true)
-      .then((res) => {
-        expect(res).to.deep.equal({data: 'Successful response from the API'});
-        done();
-      });
+  it('should return the correct data when success is true', async () => {
+    const res = await getPaymentTokenFromAPI(true);
+    expect(res).to.deep.equal({ data: 'Successful response from the API' });
   });
 });
